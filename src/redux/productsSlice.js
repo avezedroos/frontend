71716e2e899136e01fd21054fb44b61b8127e525
avezedroos <154615446+avezedroos@users.noreply.github.com>
2025-10-08 17:38:29 +0530

    Update productsSlice.js

--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -3,6 +3,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 const baseUrl = process.env.REACT_APP_API_URL;
 // const API_URL = "http://localhost:5000/api/products";
+console.log("API Base URL:", baseUrl);
 const API_URL = `${baseUrl}/api/products`;
 // Async thunk to fetch products
 export const fetchProducts = createAsyncThunk(
